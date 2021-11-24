e00d19f7f38569ac881ac65f8cb988de62e1b83a
shreya-25 <shreyaagarwal252000@gmail.com>
2021-11-24 16:06:28 +0530

    number theory started - 24 nov,2021

---
+++ src/first_file.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class first_file {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Check");
+
+    }
+}
