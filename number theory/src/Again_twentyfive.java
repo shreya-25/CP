4eb599fdda73020dbb4fa91e54442d13ebe6d3e5
shreya-25 <shreyaagarwal252000@gmail.com>
2021-11-26 22:24:13 +0530

    number theory started - 26 nov,2021

---
+++ number theory/src/Again_twentyfive.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Again_twentyfive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.println("25");
+
+    }
+}
