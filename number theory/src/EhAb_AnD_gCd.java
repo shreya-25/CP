7d2e6d17a106f52aed63ad2bb39981dccb131043
shreya-25 <shreyaagarwal252000@gmail.com>
2021-11-27 01:12:56 +0530

    number theory started - 27 nov,2021

---
+++ number theory/src/EhAb_AnD_gCd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class EhAb_AnD_gCd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            System.out.println(1 +" "+ (n-1));
+
+        }
+
+    }
+}
